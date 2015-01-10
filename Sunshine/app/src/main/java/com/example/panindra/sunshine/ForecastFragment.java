919dd4d7e7918613e40b8224e56d2277c55ab1cc
panindra1 <tumkurs2@illinois.edu>
2015-01-09 23:24:22 -0600

    Added temperature settings along with location

--- Sunshine/app/src/main/java/com/example/panindra/sunshine/ForecastFragment.java
+++ Sunshine/app/src/main/java/com/example/panindra/sunshine/ForecastFragment.java
@@ -227,6 +227,18 @@ public class ForecastFragment extends Fragment {
          */
         private String formatHighLows(double high, double low) {
             // For presentation, assume the user doesn't care about tenths of a degree.
+
+            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String unitType = sharedPrefs.getString(getString(R.string.pref_syncConnectionType), getString(R.string.pref_syncConnectionTypes_default));
+
+               if(unitType.equals(getString(R.string.pref_units_imperial))) {
+                    high = (high * 1.8) + 32;
+                    low = (low * 1.8) + 32;
+               }
+            else if(!unitType.equals(getString(R.string.pref_units_metric))) {
+                    Log.v(LOG_TAG, "Unite type not found");
+               }
+
             long roundedHigh = Math.round(high);
             long roundedLow = Math.round(low);
 
