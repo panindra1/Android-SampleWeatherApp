0cd8a751af96cd116ca596a13837c99959671402
panindra1 <tumkurs2@illinois.edu>
2015-01-10 10:34:36 -0600

    Added sharing weather data in other Apps

--- Sunshine/app/src/main/java/com/example/panindra/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/example/panindra/sunshine/MainActivity.java
@@ -9,10 +9,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.ShareActionProvider;
 
 
 public class MainActivity extends ActionBarActivity {
+    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +30,11 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
