4fe84af92fb967598746ca213c56e7d2ff7196e8
panindra1 <tumkurs2@illinois.edu>
2015-01-10 08:53:02 -0600

    Added Map location as settings

--- Sunshine/app/src/main/java/com/example/panindra/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/example/panindra/sunshine/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.panindra.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -41,10 +45,28 @@ public class MainActivity extends ActionBarActivity {
             startActivity(settingsIntent);
             return true;
         }
+        else if(id == R.id.action_map) {
+            openPreferredLocationInMap();
+        }
 
         return super.onOptionsItemSelected(item);
     }
 
+    public void openPreferredLocationInMap() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharedPreferences.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+        Uri geoLocation = Uri.parse("geo:0.0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
 
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        else {
+            Log.v("Could not call", "Could not call");
+        }
+    }
 
 }
